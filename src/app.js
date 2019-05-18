a3c3489813365eb029007d6a0e48611d026e78f1
Mateus da Silva Sousa <mateus.wtv@gmail.com>
2019-05-18 15:35:18 -0300

    fix: add style on topbar

--- src/app.js
+++ src/app.js
@@ -3,8 +3,19 @@ import { Switch, Route, Link } from "react-router-dom";
 import ProductsStorage from "./components/productsStorage";
 import ListProducts from "./components/listProducts";
 import Cart from "./components/cart";
+import styles from "./app.css";
 
 const Home = () => (
+  <div className={styles.navbar}>
+    <Link to="/" className={styles.link}>
+      Tela inicial
+    </Link>
+    <Link to="/cadastro+de+produtos" className={styles.link}>
+      Cadastrar Produtos
+    </Link>
+    <Link to="/products" className={styles.link}>
+      Listar produtos
+    </Link>
     <Link to="/carrinho" className={styles.link}>
       Carrinho
     </Link>
@@ -18,6 +29,7 @@ const app = () => {
       <Switch>
         <Route exact path="/cadastro+de+produtos" component={ProductsStorage} />
         <Route exact path="/products" component={ListProducts} />
+        <Route exact path="/carrinho" component={Cart} />
       </Switch>
     </div>
   );

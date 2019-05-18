5052193406e8bb4ab38b199d78afced3ad31efef
Mateus da Silva Sousa <mateus.wtv@gmail.com>
2019-05-18 15:36:31 -0300

    fix: fix products Storage component

--- src/components/productsStorage/index.js
+++ src/components/productsStorage/index.js
@@ -1,25 +1,59 @@
 import React from "react";
+import CurrencyInput from "react-currency-input";
 
-const submit = event => {
-  event.preventDefault();
-  const name = event.target.children[0].value;
-  const price = event.target.children[1].value;
-  const products = JSON.parse(sessionStorage.getItem("products"));
-  products.push({
-    name: name,
-    price: price
-  });
-  sessionStorage.setItem("products", JSON.stringify(products));
-};
+class productsStorage extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      price: 0.0,
+      truePrice: 0.0
+    };
+  }
+  submit = event => {
+    event.preventDefault();
+    const name = this.state.name;
+    const price = this.state.truePrice;
+    const products = JSON.parse(sessionStorage.getItem("products"));
+    products.push({
+      name: name,
+      price: price
+    });
+    sessionStorage.setItem("products", JSON.stringify(products));
+    this.setState({
+      name: "",
+      price: 0.0,
+      truePrice: 0.0
+    });
+  };
 
-const productsStorage = () => {
-  return (
-    <form onSubmit={submit}>
-      <input type="text" />
-      <input type="number" min="0.00" step="0.01" />
-      <input type="submit" value="adicionar" />
-    </form>
-  );
-};
+  changePrice = (event, maskedvalue, floatvalue) => {
+    this.setState({
+      price: maskedvalue,
+      truePrice: floatvalue
+    });
+  };
 
+  changeName = event => {
+    this.setState({
+      name: event.target.value
+    });
+  };
+
+  render = () => {
+    return (
+      <form onSubmit={this.submit}>
+        <input type="text" value={this.state.name} onChange={this.changeName} />
+        <CurrencyInput
+          value={this.state.price}
+          onChangeEvent={this.changePrice}
+          decimalSeparator=","
+          thousandSeparator="."
+          prefix="R$"
+        />
+        <input type="submit" value="adicionar" />
+      </form>
+    );
+  };
+}
 export default productsStorage;
